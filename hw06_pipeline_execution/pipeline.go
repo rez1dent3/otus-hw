5d6a32ab8e1ade4c32736251dee93f50585c1a20
Maxim Babichev <maksim.babichev95@gmail.com>
2022-07-20 17:16:26 +0300

    recursion

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,10 +1,5 @@
 package hw06pipelineexecution
 
-import (
-	"context"
-	"sync"
-)
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -14,63 +9,24 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	results := make(map[int]interface{})
-	ctx, cancel := context.WithCancel(context.Background())
-
-	var i int
-	var mx sync.Mutex
-	var wg sync.WaitGroup
-	for datum := range in {
-		wg.Add(1)
-		ch := make(Bi, 1)
-		ch <- datum
-		close(ch)
-
-		go func(item Out, i int) {
-			defer wg.Done()
-			for _, stage := range stages {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-					item = stage(item)
-				}
-			}
-
-			mx.Lock()
-			defer mx.Unlock()
-			results[i] = <-item
-		}(ch, i)
-
-		i++
+	if len(stages) == 0 {
+		return in
 	}
 
-	wait := make(chan struct{})
+	results := make(Bi)
+	stage := stages[0]
+
 	go func() {
-		wg.Wait()
-		close(wait)
-	}()
-
-	out := make(Bi)
-	go func() {
-		defer close(out)
-
-		select {
-		case <-wait:
-		case <-done:
-			cancel()
-			return
-		}
-
-		for k := 0; ; k++ {
-			v, ok := results[k]
-			if !ok {
-				break
+		defer close(results)
+		for item := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+				results <- item
 			}
-
-			out <- v
 		}
 	}()
 
-	return out
+	return ExecutePipeline(results, done, stages[1:]...)
 }
