a37352da35ac6a631cb8a676037df95bd91d105b
Maxim Babichev <maksim.babichev95@gmail.com>
2022-07-21 23:49:07 +0300

    HW6 is completed (#6)
    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,24 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	results := make(Bi)
+	stage := stages[0]
+
+	go func() {
+		defer close(results)
+		for item := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+				results <- item
+			}
+		}
+	}()
+
+	return ExecutePipeline(results, done, stages[1:]...)
 }
