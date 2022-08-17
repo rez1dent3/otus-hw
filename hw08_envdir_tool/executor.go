f65d93bb81d67891984a15d77c5817d9660b7c01
Maxim Babichev <maksim.babichev95@gmail.com>
2022-08-17 18:54:23 +0300

    HW8 is completed (#8)

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func runCmd(writer io.Writer, cmd []string, env Environment) int {
+	ind := 0
+	environ := make([]string, len(env))
+	for key, item := range env {
+		environ[ind] = fmt.Sprintf("%s=%s", key, item.Value)
+		ind++
+	}
+
+	command := exec.Cmd{
+		Path:   cmd[0],
+		Args:   cmd[0:],
+		Env:    environ,
+		Stdout: writer,
+	}
+
+	if err := command.Run(); err != nil {
+		var exit *exec.ExitError
+		if errors.As(err, &exit) {
+			return exit.ExitCode()
+		}
+	}
+
+	return 0
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmd []string, env Environment) int {
+	return runCmd(os.Stdout, cmd, env)
 }
