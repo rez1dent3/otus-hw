69d0b1365ff23fa5291682cc68f09b24f720fcf2
Maxim Babichev <maksim.babichev95@gmail.com>
2022-09-21 22:09:43 +0300

    HW12 init

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -1,3 +1,8 @@
-module github.com/fixme_my_friend/hw12_13_14_15_calendar
+module github.com/rez1dent3/otus-hw/hw12_13_14_15_calendar
 
 go 1.16
+
+require (
+	github.com/stretchr/testify v1.8.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- hw12_13_14_15_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_calendar/internal/logger/logger_test.go
@@ -1,7 +1,51 @@
-package logger
+package logger_test
 
-import "testing"
+import (
+	"bytes"
+	"github.com/stretchr/testify/require"
+	"testing"
+
+	"github.com/rez1dent3/otus-hw/hw12_13_14_15_calendar/internal/logger"
+)
 
 func TestLogger(t *testing.T) {
-	// TODO
+	t.Run("check level", func(t *testing.T) {
+		data := []struct {
+			level    string
+			expected string
+		}{
+			{
+				level:    "off",
+				expected: "",
+			},
+			{
+				level:    "error",
+				expected: "error\n",
+			},
+			{
+				level:    "warning",
+				expected: "error\nwarning\n",
+			},
+			{
+				level:    "info",
+				expected: "error\nwarning\ninfo\n",
+			},
+			{
+				level:    "debug",
+				expected: "error\nwarning\ninfo\ndebug\n",
+			},
+		}
+
+		for _, datum := range data {
+			buffer := &bytes.Buffer{}
+			log := logger.New(datum.level, buffer)
+
+			log.Error("error")
+			log.Warning("warning")
+			log.Info("info")
+			log.Debug("debug")
+
+			require.Equal(t, datum.expected, buffer.String())
+		}
+	})
 }
