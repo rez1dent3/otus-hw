6cec8a59b1aaeb00edba6526dc9fc61bd71fca5c
Maxim Babichev <maksim.babichev95@gmail.com>
2022-10-11 21:28:29 +0300

    HW12 repo

--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,7 +1,18 @@
 package storage
 
+import (
+	"github.com/rez1dent3/otus-hw/hw12_13_14_15_calendar/pkg/uuid"
+	"time"
+)
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID          uuid.UUID
+	Title       string
+	Description string
+	StartAt     time.Time
+	EndAt       time.Time
+	UserID      uuid.UUID
+	RemindFor   uint32
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,14 +1,69 @@
 package memorystorage
 
-import "sync"
+import (
+	"github.com/rez1dent3/otus-hw/hw12_13_14_15_calendar/internal/storage"
+	"github.com/rez1dent3/otus-hw/hw12_13_14_15_calendar/pkg/uuid"
+	"sync"
+	"time"
+)
 
 type Storage struct {
-	// TODO
-	mu sync.RWMutex
+	events map[uuid.UUID]storage.Event
+	mu     sync.RWMutex
 }
 
 func New() *Storage {
 	return &Storage{}
 }
 
-// TODO
+func (s *Storage) CreateEvent(event storage.Event) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.events[event.ID] = event
+
+	return true
+}
+
+func (s *Storage) UpdateEvent(eventId uuid.UUID, event storage.Event) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, ok := s.events[eventId]; !ok {
+		return false
+	}
+
+	s.events[eventId] = event
+
+	return true
+}
+
+func (s *Storage) DeleteEvent(eventId uuid.UUID) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	delete(s.events, eventId)
+
+	return true
+}
+
+func (s *Storage) ListEventToday(date time.Time) []storage.Event {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return []storage.Event{}
+}
+
+func (s *Storage) ListEventWeek(date time.Time) []storage.Event {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return []storage.Event{}
+}
+
+func (s *Storage) ListEventMonth(date time.Time) []storage.Event {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return []storage.Event{}
+}
