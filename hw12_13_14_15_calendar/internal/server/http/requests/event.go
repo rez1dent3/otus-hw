aba8984ce65962b4b205cee062bc4996d9ef2d2a
Maxim Babichev <maksim.babichev95@gmail.com>
2022-11-02 22:25:10 +0300

    HW13 http tests

--- hw12_13_14_15_calendar/internal/server/http/requests/event.go
+++ hw12_13_14_15_calendar/internal/server/http/requests/event.go
@@ -5,16 +5,16 @@ import (
 )
 
 type EventRequest struct {
-	ID          string
-	Title       string
-	Description string
-	StartAt     time.Time
-	EndAt       time.Time
-	UserID      string
-	RemindFor   uint32
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	StartAt     time.Time `json:"start_at"`
+	EndAt       time.Time `json:"end_at"`
+	UserID      string    `json:"user_id"`
+	RemindFor   uint32    `json:"remind_for"`
 }
 
 type EventListRequest struct {
-	UserID string
-	Date   time.Time
+	UserID string    `json:"user_id"`
+	Date   time.Time `json:"date"`
 }
