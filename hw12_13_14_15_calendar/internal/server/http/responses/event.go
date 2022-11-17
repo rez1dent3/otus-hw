3911b45a773f3ca37a81a54861b93ffcb2533bbb
Maxim Babichev <maksim.babichev95@gmail.com>
2022-11-17 22:21:01 +0300

    HW15 http server update response

--- hw12_13_14_15_calendar/internal/server/http/responses/event.go
+++ hw12_13_14_15_calendar/internal/server/http/responses/event.go
@@ -3,13 +3,14 @@ package responses
 import "time"
 
 type EventResponse struct {
-	ID          string         `json:"id"`
-	Title       string         `json:"title"`
-	Description *string        `json:"description"`
-	StartAt     time.Time      `json:"start_at"`
-	EndAt       time.Time      `json:"end_at"`
-	UserID      string         `json:"user_id"`
-	RemindFor   *time.Duration `json:"remind_for"`
+	ID           string         `json:"id"`
+	Title        string         `json:"title"`
+	Description  *string        `json:"description"`
+	StartAt      time.Time      `json:"start_at"`
+	EndAt        time.Time      `json:"end_at"`
+	UserID       string         `json:"user_id"`
+	RemindFor    *time.Duration `json:"remind_for"`
+	IsDispatched bool           `json:"is_dispatched"`
 }
 
 type ErrorResponse struct {
